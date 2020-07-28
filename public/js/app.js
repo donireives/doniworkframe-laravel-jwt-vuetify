5e6fb5f235d48dcb763710998f92d49379e30bd7
doniReives <doni4869@gmail.com>
2020-07-28 07:55:56 +0700

    vuetify installed

--- public/js/app.js
+++ public/js/app.js
@@ -87355,8 +87355,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! d:\doni_server\WinNMP\WWW\mirror-dev\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! d:\doni_server\WinNMP\WWW\mirror-dev\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! d:\doni_server\WinNMP\WWW\doniworkframe-laravel-jwt-vuetify\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! d:\doni_server\WinNMP\WWW\doniworkframe-laravel-jwt-vuetify\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
